1e80501b9eb4d3527fe175c35fded965fde39926
inqilobchi <hieojody@gmail.com>
2025-10-02 16:35:54 +0500

    Update Home.jsx

--- frontEnd/src/pages/home/Home.jsx
+++ frontEnd/src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ const Home = ({ type }) => {
 		const getRandomLists = async () => {
 			try {
 				const res = await axios.get(
-					`lists${type ? '?type' + type : ''}${
+					`https://netflixclone-dnkc.onrender.com/lists${type ? '?type' + type : ''}${
 						genre ? '&genre=' + genre : ''
 					}`,
 					{
